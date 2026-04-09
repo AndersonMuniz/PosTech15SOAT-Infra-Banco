8ca6be928e50a53b2a22e68594dce1f79ae69935
Marcelo Moreira <marcelogilosmoreira@gmail.com>
2026-04-08 22:33:13 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/numberone/NumberoneApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.numberone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberoneApplication.class, args);
+	}
+
+}
