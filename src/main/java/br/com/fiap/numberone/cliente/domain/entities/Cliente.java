66061ab7ec4e340433faf474f516aab602670cdd
AndersonMuniz <46923238+AndersonMuniz@users.noreply.github.com>
2026-04-18 13:51:22 -0300

    Refatora Cliente para separar entidade de persistencia

--- src/main/java/br/com/fiap/numberone/cliente/domain/entities/Cliente.java
+++ src/main/java/br/com/fiap/numberone/cliente/domain/entities/Cliente.java
@@ -1,7 +1,6 @@
 package br.com.fiap.numberone.cliente.domain.entities;
 
 import br.com.fiap.numberone.cliente.domain.enums.TipoDocumento;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,53 +8,21 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Entity
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "cliente")
 public class Cliente {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(nullable = false, length = 90)
     private String nome;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "tipo_documento", nullable = false, length = 30)
     private TipoDocumento tipoDocumento;
-
-    @Column(nullable = false, length = 50)
     private String documento;
-
-    @Column(nullable = false, length = 15)
     private String telefone;
-
-    @Column(nullable = false, length = 90)
     private String endereco;
-
-    @Column(nullable = false)
     private Boolean ativo;
-
-    @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
-
-    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
-
-    @PrePersist
-    public void prePersist() {
-        this.createdAt = LocalDateTime.now();
-        this.ativo = this.ativo == null ? Boolean.TRUE : this.ativo;
-    }
-
-    @PreUpdate
-    public void preUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
 
     public Cliente updateFrom(Cliente novoCliente) {
         return Cliente.builder()
--- src/main/java/br/com/fiap/numberone/cliente/infrastructure/repositories/ClienteRepository.java
+++ src/main/java/br/com/fiap/numberone/cliente/infrastructure/repositories/ClienteRepository.java
@@ -1,11 +1,9 @@
 package br.com.fiap.numberone.cliente.infrastructure.repositories;
 
-import br.com.fiap.numberone.cliente.domain.entities.Cliente;
+import br.com.fiap.numberone.cliente.infrastructure.persistence.entities.ClienteEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
+public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
 }
