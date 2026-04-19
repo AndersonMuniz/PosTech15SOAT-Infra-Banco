812e54cc85705ad606fcb4aa8e003187537dbf87
Julio Cesar <julioosilva97@gmail.com>
2026-04-19 00:29:30 -0300

    fix: ajustes para rodar local, depois dono da fetature ajustar

--- src/main/java/br/com/fiap/numberone/veiculo/domain/entities/Veiculo.java
+++ src/main/java/br/com/fiap/numberone/veiculo/domain/entities/Veiculo.java
@@ -1,15 +1,30 @@
 package br.com.fiap.numberone.veiculo.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Veiculo {
 
-    private UUID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Transient
     private String placa;
+    @Transient
     private String marca;
+    @Transient
     private String modelo;
+    @Transient
     private Integer ano;
+    @Transient
     private String idCliente;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
