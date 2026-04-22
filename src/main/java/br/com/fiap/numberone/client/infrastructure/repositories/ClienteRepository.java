f577812bab9c373211da2f4abdfe586b81efe5a1
AndersonMuniz <anderson_c_m@yahoo.com.br>
2026-04-22 20:42:25 -0300

    ajuste no nome das tabelas

--- src/main/java/br/com/fiap/numberone/client/infrastructure/repositories/ClienteRepository.java
+++ src/main/java/br/com/fiap/numberone/client/infrastructure/repositories/ClienteRepository.java
@@ -1,11 +1,11 @@
 package br.com.fiap.numberone.client.infrastructure.repositories;
 
-import br.com.fiap.numberone.client.infrastructure.persistence.entities.ClienteEntity;
+import br.com.fiap.numberone.client.infrastructure.persistence.entities.ClientEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<ClienteEntity, UUID> {
+public interface ClienteRepository extends JpaRepository<ClientEntity, UUID> {
 }
