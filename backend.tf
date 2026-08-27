43d6674cc414cc4c97c6481216f18868dd60c5de
AndersonMuniz <46923238+AndersonMuniz@users.noreply.github.com>
2026-08-26 21:47:36 -0300

    define backend S3 dedicado para AWS Academy

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "backend-terraform-numberone"
+    bucket = "postech15soat-infra-banco-tfstate-777137014941"
     key    = "infra-banco/terraform.tfstate"
     region = "us-east-1"
   }
